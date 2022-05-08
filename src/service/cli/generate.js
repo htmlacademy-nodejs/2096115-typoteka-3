55413bfe1f312f4c13697c64ddd1f110ad553b90
Pavel Bologan <p.bologan@gmail.com>
2022-05-08 13:00:50 +0300

    add titles, categories, sentences params to the generatePosts function

--- src/service/cli/generate.js
+++ src/service/cli/generate.js
@@ -4,16 +4,21 @@ const chalk = require(`chalk`);
 const fs = require(`fs`).promises;
 const {getRandomInt, shuffle, subtractMonths, getFormattedDateString} = require(`./utils/utils`);
 const {
-  CATEGORIES, TITLES, SENTENCES, DEFAULT_COUNT, FILE_NAME, ExitCode,
+  DEFAULT_COUNT,
+  FILE_NAME,
+  FILE_SENTENCES_PATH,
+  FILE_TITLES_PATH,
+  FILE_CATEGORIES_PATH,
+  ExitCode,
 } = require(`./utils/constants`);
 
-const generatePosts = (count) => (
+const generatePosts = (count, titles, categories, sentences) => (
   Array.from({length: count}, () => ({
-    title: TITLES[getRandomInt(0, TITLES.length - 1)],
+    title: titles[getRandomInt(0, titles.length - 1)],
     createdDate: getFormattedDateString(subtractMonths(new Date(), getRandomInt(0, 3))),
-    announce: shuffle(SENTENCES).slice(getRandomInt(0, 4), 5).join(` `),
-    fullText: shuffle(SENTENCES).slice(getRandomInt(0, 5), 10).join(` `),
-    category: shuffle(CATEGORIES).slice(0, getRandomInt(1, 3)),
+    announce: shuffle(sentences).slice(getRandomInt(0, 4), 5).join(` `),
+    fullText: shuffle(sentences).slice(getRandomInt(0, 5), 10).join(` `),
+    category: shuffle(categories).slice(0, getRandomInt(1, 3)),
   }))
 );
 
@@ -29,6 +34,10 @@ const readContent = async (path) => {
 module.exports = {
   name: `--generate`,
   async run(args) {
+    const sentences = await readContent(FILE_SENTENCES_PATH);
+    const titles = await readContent(FILE_TITLES_PATH);
+    const categories = await readContent(FILE_CATEGORIES_PATH);
+
     const [count] = args;
     const countOffer = Number.parseInt(count, 10) || DEFAULT_COUNT;
 
@@ -36,14 +45,14 @@ module.exports = {
       throw new Error(`Не больше 1000 публикаций`);
     }
 
-    const content = JSON.stringify(generatePosts(countOffer));
+    const content = JSON.stringify(generatePosts(countOffer, titles, categories, sentences));
 
     try {
       await fs.writeFile(FILE_NAME, content);
       console.info(chalk.green(`File has been created!`));
       process.exit(ExitCode.success);
     } catch (error) {
-      throw new Error(`Can't write file`);
+      throw new Error(chalk.red(`Can't write file`));
     }
   }
 }
