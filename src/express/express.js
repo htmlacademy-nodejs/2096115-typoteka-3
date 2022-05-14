a4f9415deb7e50939b8ac5ca90cc714c3e989e7d
Pavel Bologan <p.bologan@gmail.com>
2022-05-14 10:36:43 +0300

    add routes

--- src/express/express.js
+++ src/express/express.js
@@ -1,8 +1,17 @@
-'use strict'
+'use strict';
 
 const express = require(`express`);
+
+const mainRoutes = require(`./routes/main-routes`);
+const myRoutes = require(`./routes/my-routes`);
+const articlesRoutes = require(`./routes/articles-routes`);
+
 const DEFAULT_PORT = 8080;
 
 const app = express();
+
+app.use(`/`, mainRoutes);
+app.use(`/my`, myRoutes);
+app.use(`/articles`, articlesRoutes);
 
 app.listen(DEFAULT_PORT, () => console.log(`Сервер запущен на порту: ${DEFAULT_PORT}`));
